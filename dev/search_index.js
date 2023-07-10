a262b8334a485e3271513bd56bb9e6660998e6f9
Documenter.jl <documenter@juliadocs.github.io>
2023-07-10 10:48:04 +0000

    build based on d996c39

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IgDL","category":"page"},{"location":"#IgDL","page":"Home","title":"IgDL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IgDL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IgDL]","category":"page"}]
+}
